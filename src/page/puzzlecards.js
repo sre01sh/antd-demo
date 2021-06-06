eb680d9628013bc6abe0fb0c11f63a81161a04b9
Frank <sre01sh@163.com>
2021-06-06 21:20:53 +0800

    init

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -1,45 +1,36 @@
 import React, { Component } from 'react';
-import { Card,Button } from 'antd';
+import { Card /* ,Button */ } from 'antd';
+import { connect } from 'dva';
 
+const namespace = 'puzzlecards';
+
+const mapStateToProps = (state) => {
+  const cardList = state[namespace].data;
+  return {
+    cardList,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDidMount: () => {
+      dispatch({
+        type: `${namespace}/queryInitCards`,
+      });
+    },
+  };
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class PuzzleCardsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cardList: [
-        {
-          id: 1,
-          setup: 'Did you hear about the two silk worms in a race?',
-          punchline: 'It ended in a tie',
-        },
-        {
-          id: 2,
-          setup: 'What happens to a frog\'s car when it breaks down?',
-          punchline: 'It gets toad away',
-        },
-      ],
-    }
+  componentDidMount() {
+    this.props.onDidMount();
   }
-
-  addNewCard = () => {
-    this.setState(prevState => {
-      const prevCardList = prevState.cardList;
-      this.counter += 1;
-      const card = {
-        id: this.counter,
-        setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-        punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      };
-      return {
-        cardList: prevCardList.concat(card),
-      };
-    });
-  }
-
   render() {
     return (
       <div>
         {
-          this.state.cardList.map(card => {
+          this.props.cardList.map(card => {
             return (
               <Card key={card.id}>
                 <div>Q: {card.setup}</div>
@@ -50,9 +41,6 @@ export default class PuzzleCardsPage extends Component {
             );
           })
         }
-         <div>
-          <Button onClick={this.addNewCard}> 添加卡片 </Button>
-        </div>
       </div>
     );
   }
